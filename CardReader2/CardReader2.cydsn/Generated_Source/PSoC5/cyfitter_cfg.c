b0c7f54aa1223f5b5c32b3c62f0136202e840626
Ian Hartwig <spacepenguine@gmail.com>
2016-01-21 22:04:29 -0500

    add UART with basic parsing for RFID reader messages

--- CardReader2/CardReader2.cydsn/Generated_Source/PSoC5/cyfitter_cfg.c
+++ CardReader2/CardReader2.cydsn/Generated_Source/PSoC5/cyfitter_cfg.c
@@ -124,7 +124,7 @@ static void CyClockStartupError(uint8 errorCode)
 }
 #endif
 
-#define CY_CFG_BASE_ADDR_COUNT 12u
+#define CY_CFG_BASE_ADDR_COUNT 18u
 CYPACKED typedef struct
 {
 	uint8 offset;
@@ -132,16 +132,25 @@ CYPACKED typedef struct
 } CYPACKED_ATTR cy_cfg_addrvalue_t;
 
 #define cy_cfg_addr_table ((const uint32 CYFAR *)0x48000000u)
-#define cy_cfg_data_table ((const cy_cfg_addrvalue_t CYFAR *)0x48000030u)
+#define cy_cfg_data_table ((const cy_cfg_addrvalue_t CYFAR *)0x48000048u)
+
+/* UDB_0_1_0_CONFIG Address: CYDEV_UCFG_B1_P4_U1_BASE Size (bytes): 128 */
+#define BS_UDB_0_1_0_CONFIG_VAL ((const uint8 CYFAR *)0x48000294u)
+
+/* IOPINS0_7 Address: CYREG_PRT12_DR Size (bytes): 10 */
+#define BS_IOPINS0_7_VAL ((const uint8 CYFAR *)0x48000314u)
 
 /* IOPINS0_8 Address: CYREG_PRT15_DR Size (bytes): 10 */
-#define BS_IOPINS0_8_VAL ((const uint8 CYFAR *)0x48000050u)
+#define BS_IOPINS0_8_VAL ((const uint8 CYFAR *)0x48000320u)
 
 /* IOPINS0_1 Address: CYREG_PRT1_DM0 Size (bytes): 8 */
-#define BS_IOPINS0_1_VAL ((const uint8 CYFAR *)0x4800005Cu)
+#define BS_IOPINS0_1_VAL ((const uint8 CYFAR *)0x4800032Cu)
+
+/* IOPINS0_2 Address: CYREG_PRT2_DM0 Size (bytes): 8 */
+#define BS_IOPINS0_2_VAL ((const uint8 CYFAR *)0x48000334u)
 
 /* CYDEV_CLKDIST_ACFG0_CFG0 Address: CYREG_CLKDIST_ACFG0_CFG0 Size (bytes): 4 */
-#define BS_CYDEV_CLKDIST_ACFG0_CFG0_VAL ((const uint8 CYFAR *)0x48000064u)
+#define BS_CYDEV_CLKDIST_ACFG0_CFG0_VAL ((const uint8 CYFAR *)0x4800033Cu)
 
 
 /*******************************************************************************
@@ -200,6 +209,10 @@ static void ClockSetup(void)
 	uint8 pllLock;
 
 
+	/* Configure Digital Clocks based on settings from Clock DWR */
+	CY_SET_XTND_REG16((void CYFAR *)(CYREG_CLKDIST_DCFG0_CFG0), 0x00D6u);
+	CY_SET_XTND_REG8((void CYFAR *)(CYREG_CLKDIST_DCFG0_CFG0 + 0x2u), 0x18u);
+
 	/* Configure Analog Clocks based on settings from Clock DWR */
 	CYCONFIGCPY((void CYFAR *)(CYREG_CLKDIST_ACFG0_CFG0), (const void CYFAR *)(BS_CYDEV_CLKDIST_ACFG0_CFG0_VAL), 4u);
 
@@ -236,6 +249,8 @@ static void ClockSetup(void)
 	/* Configure USB Clock based on settings from Clock DWR */
 	CY_SET_XTND_REG8((void CYFAR *)(CYREG_CLKDIST_UCFG), 0x00u);
 	CY_SET_XTND_REG8((void CYFAR *)(CYREG_CLKDIST_LD), 0x02u);
+
+	CY_SET_XTND_REG8((void CYFAR *)(CYREG_PM_ACT_CFG2), ((CY_GET_XTND_REG8((void CYFAR *)CYREG_PM_ACT_CFG2) | 0x01u)));
 }
 
 
@@ -335,16 +350,28 @@ void cyfitter_cfg(void)
 			uint16 size;
 		} CYPACKED_ATTR cfg_memset_t;
 
+
+		CYPACKED typedef struct {
+			void CYFAR *dest;
+			const void CYFAR *src;
+			uint16 size;
+		} CYPACKED_ATTR cfg_memcpy_t;
+
 		static const cfg_memset_t CYCODE cfg_memset_list [] = {
 			/* address, size */
 			{(void CYFAR *)(CYREG_PRT0_DR), 16u},
-			{(void CYFAR *)(CYREG_PRT2_DR), 80u},
-			{(void CYFAR *)(CYREG_PRT12_DR), 16u},
+			{(void CYFAR *)(CYREG_PRT3_DR), 64u},
 			{(void CYFAR *)(CYDEV_UCFG_B0_P0_U0_BASE), 4096u},
-			{(void CYFAR *)(CYDEV_UCFG_B1_P2_U0_BASE), 2048u},
+			{(void CYFAR *)(CYDEV_UCFG_B1_P2_U0_BASE), 1152u},
+			{(void CYFAR *)(CYDEV_UCFG_B1_P4_ROUTE_BASE), 768u},
 			{(void CYFAR *)(CYDEV_UCFG_DSI0_BASE), 2560u},
 			{(void CYFAR *)(CYDEV_UCFG_DSI12_BASE), 512u},
 			{(void CYFAR *)(CYREG_BCTL0_MDCLK_EN), 32u},
+		};
+
+		static const cfg_memcpy_t CYCODE cfg_memcpy_list [] = {
+			/* dest, src, size */
+			{(void CYFAR *)(CYDEV_UCFG_B1_P4_U1_BASE), BS_UDB_0_1_0_CONFIG_VAL, 128u},
 		};
 
 		uint8 CYDATA i;
@@ -354,6 +381,16 @@ void cyfitter_cfg(void)
 		{
 			const cfg_memset_t CYCODE * CYDATA ms = &cfg_memset_list[i];
 			CYMEMZERO(ms->address, (size_t)(uint32)(ms->size));
+		}
+
+		/* Copy device configuration data into registers */
+		for (i = 0u; i < (sizeof(cfg_memcpy_list)/sizeof(cfg_memcpy_list[0])); i++)
+		{
+			const cfg_memcpy_t CYCODE * CYDATA mc = &cfg_memcpy_list[i];
+			void * CYDATA destPtr = mc->dest;
+			const void * CYDATA srcPtr = mc->src;
+			uint16 CYDATA numBytes = mc->size;
+			CYCONFIGCPY(destPtr, srcPtr, numBytes);
 		}
 
 		cfg_write_bytes32(cy_cfg_addr_table, cy_cfg_data_table);
@@ -369,8 +406,10 @@ void cyfitter_cfg(void)
 
 
 	/* Perform second pass device configuration. These items must be configured in specific order after the regular configuration is done. */
+	CYCONFIGCPY((void CYFAR *)(CYREG_PRT12_DR), (const void CYFAR *)(BS_IOPINS0_7_VAL), 10u);
 	CYCONFIGCPY((void CYFAR *)(CYREG_PRT15_DR), (const void CYFAR *)(BS_IOPINS0_8_VAL), 10u);
 	CYCONFIGCPY((void CYFAR *)(CYREG_PRT1_DM0), (const void CYFAR *)(BS_IOPINS0_1_VAL), 8u);
+	CYCONFIGCPY((void CYFAR *)(CYREG_PRT2_DM0), (const void CYFAR *)(BS_IOPINS0_2_VAL), 8u);
 	/* Switch Boost to the precision bandgap reference from its internal reference */
 	CY_SET_REG8((void CYXDATA *)CYREG_BOOST_CR2, (CY_GET_REG8((void CYXDATA *)CYREG_BOOST_CR2) | 0x08u));
 
--- CardReader2/CardReader2.cydsn/main.c
+++ CardReader2/CardReader2.cydsn/main.c
@@ -12,19 +12,152 @@
 *******************************************************************************/
 
 #include <project.h>
+#include <stdio.h>
+#include <string.h>
 #include "kb_usb_hid.h"
 
+#define DEBUG
 
 /** @brief global buffer for keyboard led state */
 uint8_t g_led_state = 0x0;
+
+/** @brief max length of an RFID response packet we care about */
+#define RFID_PACKET_LEN 15
+#define RFID_HEADER 0xFF
+#define RFID_RESERVED 0x00
+#define UART_GET_ERROR(b) ((uint16_t)b >> 8)
+#define UART_GET_DATA(b) (b & 0x00FF)
+
+/** @brief uart packet to ask rfid reader to seek for a card */
+const uint8_t rfid_command_seek[] = {0xFF, 0x00, 0x01, 0x82, 0x83};
+
+/** @brief length of */
+
+typedef enum rfid_meta {
+    RFID_META_NONE = 0,
+    RFID_META_HEADER,
+    RFID_META_RESERVED,
+    RFID_META_LENGTH,
+    RFID_META_DATA,
+    RFID_META_CRC,
+} rfid_meta_t;
+
+
+void rfid_read_packet(uint8_t *buffer, int length_max, int *length_seen_p) {
+    #ifdef DEBUG
+    char display_buffer[1024];
+    #endif
+    
+    // iteratively process returned packets
+    rfid_meta_t rfid_meta_seen = RFID_META_NONE;
+    unsigned int rfid_length_expected;
+    unsigned int rfid_length_seen;
+    while(1) {
+        // wait for a new character
+        while(UART_GetRxBufferSize() == 0) {}
+        // check this byte for hw problems
+        uint16_t new_byte = UART_GetByte();
+        uint8_t new_byte_error = UART_GET_ERROR(new_byte);
+        uint8_t new_byte_data = UART_GET_DATA(new_byte);
+        if(UART_GET_ERROR(new_byte) != 0) {
+            #ifdef DEBUG
+            snprintf(display_buffer, 1024, "Got byte 0x%02x with error code 0x%02x!\n",
+                     new_byte_data, new_byte_error);
+            kb_puts(display_buffer);
+            #endif
+            // reset UART packet state machine
+            rfid_meta_seen = RFID_META_NONE;
+        }
+        else {
+            // UART packet from RFID state machine
+            switch(rfid_meta_seen) {
+                case RFID_META_NONE:
+                    if(new_byte_data == RFID_HEADER) {
+                        rfid_meta_seen = RFID_META_HEADER;
+                    }
+                    // no need to reset. already at first state
+                break;
+                // expect to see reserved next
+                case RFID_META_HEADER:
+                    if(new_byte_data == RFID_RESERVED) {
+                        rfid_meta_seen = RFID_META_RESERVED;
+                    }
+                    // reset if we don't see a reserved byte
+                    else {
+                        rfid_meta_seen = RFID_META_NONE;
+                    }
+                break;
+                // expect to see length next
+                case RFID_META_RESERVED:
+                    if(new_byte_data <= length_max) {
+                        rfid_length_expected = new_byte_data;
+                        rfid_length_seen = 0;
+                        rfid_meta_seen = RFID_META_LENGTH;
+                    }
+                    // reset if we see a length longer than we care about
+                    else {
+                        rfid_meta_seen = RFID_META_NONE;
+                    }
+                break;
+                // expect to read in data next
+                case RFID_META_LENGTH:
+                    buffer[rfid_length_seen] = new_byte_data;
+                    rfid_length_seen++;
+                    // check for end of data. doesn't work on 0 data length
+                    if(rfid_length_seen >= rfid_length_expected) {
+                        rfid_meta_seen = RFID_META_DATA;
+                        *length_seen_p = rfid_length_seen;
+                    }
+                break;
+                // expect to see the CRC byte next
+                case RFID_META_DATA:
+                    // TODO: check CRC
+                    #ifdef DEBUG
+                    snprintf(display_buffer, 1024, "Got message type 0x%02x with length 0x%02x.\n",
+                             buffer[0], rfid_length_expected);
+                    kb_puts(display_buffer);
+                    #endif
+                    // send the packet back for processing
+                    return;
+                break;
+                // hopefully we don't get here! reset parser.
+                case RFID_META_CRC:
+                default:
+                    rfid_meta_seen = RFID_META_NONE;
+                break;
+            }
+        }
+    }
+}
 
 
 int main()
 {
     CyGlobalIntEnable;
-    kb_init();
+    LED1_Write(0xFFu);
     
-    kb_puts("I can type anything and everything!!! =)");
+    kb_init();
+    UART_Start();
+    UART_ClearRxBuffer();
+    UART_ClearTxBuffer();
+            
+    // char display_buffer[1024];
+    uint8_t rfid_packet_buffer[RFID_PACKET_LEN];
+    int rfid_packet_length;
+    
+    while(1) {
+        // set the RFID reader to seek a new card
+        UART_PutArray(rfid_command_seek, sizeof(rfid_command_seek));
+        #ifdef DEBUG
+        kb_puts("Seeking for tag...\n");
+        #endif
+        
+        rfid_packet_length = 0;
+        while(rfid_packet_length != 6) {
+            rfid_read_packet(rfid_packet_buffer, RFID_PACKET_LEN, &rfid_packet_length);
+        }
+    }
+    
     while(1) {}
 }
 
